a109594933529c7886b94e1fec33992572deccd7
Bolebo67 <bolebo7@gmail.com>
2023-05-12 13:40:50 +0200

    first commit

---
+++ app/src/main/java/com/example/myapplication/dashboard.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+lateinit var welcomeText: String;
+lateinit var tvWelcome: TextView
+
+
+class dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        welcomeText ="Welcome "+ getIntent().getStringExtra("Username").toString() + "!";
+        tvWelcome = this.findViewById(R.id.tvWelcome);
+        tvWelcome.setText(welcomeText);
+
+
+    }
+
+}
